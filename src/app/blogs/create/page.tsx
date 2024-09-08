e1dd48edad17e66eb834c9db2b943b0947cf2559
iamRazzakk <rakibt23p@gmail.com>
2024-09-08 19:03:09 -0400

    create multipul work

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,11 @@
+
+import CreateBlogForm from "../../../components/ui/BlogForm"
+const BlogForm = () => {
+  return (
+    <div>
+      <CreateBlogForm />
+    </div>
+  )
+}
+
+export default BlogForm
